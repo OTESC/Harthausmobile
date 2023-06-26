113cc64b37a258d5e8b5b0192527013061e893f5
Akins20 <ogunbiye@gmail.com>
2023-06-26 17:56:39 +0100

    second commit

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, Welcome is Harthaus Mobile</Text>
       <StatusBar style="auto" />
     </View>
   );
